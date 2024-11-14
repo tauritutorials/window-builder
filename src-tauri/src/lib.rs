ee770e1804bd5fe4f536c775cffd7d99799e3d3b
Christian <christianpavilonis@outlook.com>
2024-11-13 18:52:32 -0600

    basic new window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,17 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::{AppHandle, WebviewUrl, WebviewWindowBuilder};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn new_window(app: AppHandle) {
+    WebviewWindowBuilder::new(&app, "window-1", WebviewUrl::App("index.html".into()))
+        .build()
+        .unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![new_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+window.addEventListener("DOMContentLoaded", () => { 
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
-}
+    document.querySelector("#new-window").addEventListener("click", () => {
+        invoke('new_window');
+    });
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
 });
