30bd328e5f787c19cec98fc38b4f6f74ad206503
Christian <christianpavilonis@outlook.com>
2024-11-14 08:37:10 -0600

    working on playing with other window options

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,20 +1,9 @@
-use tauri::{AppHandle, Manager, WebviewUrl, WebviewWindowBuilder};
+use tauri::{
+    utils::config::WindowEffectsConfig, window::Effect, AppHandle, Manager, WebviewUrl, WebviewWindowBuilder
+};
 
-#[tauri::command]
-fn new_window_or_focus(app: AppHandle) -> tauri::Result<()> {
-    match app.webview_windows().get("new-window") {
-        None => {
-            WebviewWindowBuilder::new(&app, "new-window", WebviewUrl::App("index.html".into()))
-                .build()?;
-        }
-        Some(window) => {
-            window.set_focus()?;
-        }
-    }
-
-    Ok(())
-}
-
+/// this will create a new window every time it is called using the length of the webview_windows()
+/// HashMap to create a unique label for each window.
 #[tauri::command]
 fn new_window(app: AppHandle) -> tauri::Result<()> {
     let len = app.webview_windows().len();
@@ -29,11 +18,52 @@ fn new_window(app: AppHandle) -> tauri::Result<()> {
     Ok(())
 }
 
+/// attempts to get the window with label focus, if it does it will set the focus to that window,
+/// otherwise it will create a brand new window.
+#[tauri::command]
+fn new_window_or_focus(app: AppHandle) -> tauri::Result<()> {
+    match app.webview_windows().get("focus") {
+        None => {
+            WebviewWindowBuilder::new(&app, "focus", WebviewUrl::App("index.html".into()))
+                .build()?;
+        }
+        Some(window) => {
+            window.set_focus()?;
+        }
+    }
+
+    Ok(())
+}
+
+#[tauri::command]
+fn options(app: AppHandle) -> tauri::Result<()> {
+    WebviewWindowBuilder::new(&app, "options", WebviewUrl::App("effects.html".into()))
+        .title("Custom title")
+        .resizable(false)
+        .theme(Some(tauri::Theme::Dark)) // effects other windows also.
+        .decorations(false)
+        .closable(true)
+        .transparent(true)
+        .effects(WindowEffectsConfig {
+            effects: vec![tauri::utils::WindowEffect::HudWindow],
+            state: None,
+            radius: Some(14.0),
+            color: None,
+        })
+        .build()?;
+
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![new_window, new_window_or_focus])
+        .invoke_handler(tauri::generate_handler![
+            new_window,
+            new_window_or_focus,
+            options
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 const { invoke } = window.__TAURI__.core;
 
-window.addEventListener("DOMContentLoaded", () => { 
+window.addEventListener("DOMContentLoaded", async () => {
     document.querySelector("#new-window").addEventListener("click", () => {
-        invoke('new_window');
+        invoke("new_window");
     });
+
+    document
+        .querySelector("#new-window-or-focus")
+        .addEventListener("click", () => {
+            invoke("new_window_or_focus");
+        });
+
+
+    document
+        .querySelector("#options")
+        .addEventListener("click", () => {
+            invoke("options");
+        });
 });
